0b5924aca8d6885e7249e5a8d13e752a6b5e906c
Sylvain Boily <sylvainboilydroid@gmail.com>
2017-04-05 10:31:07 -0400

    fix copy/paste

--- context/view.py
+++ context/view.py
@@ -11,6 +11,6 @@ class ContextDestinationView(BaseDestinationView):
 
     def list_json(self):
         params = self._extract_params()
-        ivrs = self.service.list(**params)
-        results = [{'id': ivr['id'], 'text': ivr['name']} for ivr in ivrs['items']]
-        return self._select2_response(results, ivr['total'], params)
+        contexts = self.service.list(**params)
+        results = [{'id': context['id'], 'text': context['name']} for context in contexts['items']]
+        return self._select2_response(results, contexts['total'], params)
