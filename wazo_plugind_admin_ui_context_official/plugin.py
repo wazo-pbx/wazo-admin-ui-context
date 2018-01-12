a3e450306801b3ab2821db24b99472b34c035619
Cedric Abunar <cabunar@wazo.io>
2018-01-12 08:48:34 -0500

    add form context

--- wazo_plugind_admin_ui_context_official/plugin.py
+++ wazo_plugind_admin_ui_context_official/plugin.py
@@ -1,11 +1,13 @@
 # Copyright 2017 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0+
 
+from flask_menu.classy import register_flaskview
+
 from wazo_admin_ui.helpers.plugin import create_blueprint
 from wazo_admin_ui.helpers.destination import register_listing_url
 
 from .service import ContextService
-from .view import ContextListingView
+from .view import ContextView, ContextListingView
 
 context = create_blueprint('context', __name__)
 
@@ -14,6 +16,10 @@ class Plugin(object):
 
     def load(self, dependencies):
         core = dependencies['flask']
+
+        ContextView.service = ContextService()
+        ContextView.register(context, route_base='/contexts')
+        register_flaskview(context, ContextView)
 
         ContextListingView.service = ContextService()
         ContextListingView.register(context, route_base='/contexts_listing')
